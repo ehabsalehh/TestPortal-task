383eae34ccb6223cc164cffabf91d7b088ee8812
ehabsalehh <ehabEbrahemSaleh.com>
2022-08-05 16:48:40 +0200

    .

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,6 @@ use App\Http\Controllers\EmployeeController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-// Route::resource("/employees",EmployeeController::class);
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
